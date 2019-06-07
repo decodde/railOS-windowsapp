4468db7cfcdff4b5d9be9bc2d69611626e42c868
Fasina Ore <dannyoma75@gmail.com>
2019-06-07 13:57:13 +0100

    splash screen

--- mainrenderer.js
+++ mainrenderer.js
@@ -1,17 +1,31 @@
+"use strict"
 const electron=require("electron")
 const app=electron.app
 const window=electron.BrowserWindow
-var mainWindow=null
-
+let mainWindow=null
+let splash=null
 const path = require('path')
 const url = require('url')
+let fileURL="/views/splashscreen.html"
 
-let createWindow=(w,fileURL,width,height)=>{
-    w=new window({width:width,height:height})
-    w.loadURL(url.format({
-        pathname: path.join(__dirname, ),
+function createWindow(){
+    splash=new window({width:400,height:300,frame:false})
+    splash.loadURL(url.format({
+        pathname: path.join(__dirname, fileURL),
         protocol: 'file:',
         slashes: true
     }))
+    mainWindow=new window({width:1024,height:768,show:false})
+        mainWindow.loadURL(url.format({
+            pathname: path.join(__dirname, "/views/index.html"),
+            protocol: 'file:',
+            slashes: true
+        }))
+    setTimeout(() => {
+        splash.destroy()
+        mainWindow.show()
+    }, 6000);
+    
+    
 }
-app.on('ready',createWindow(mainWindow,'/views/index.html',"1024","768"))
+app.on('ready',createWindow)
