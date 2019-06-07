15ec996bf2483f31a0c8e1550c428c79fbc77076
Fasina Oreoluwa <dannyoma75@gmail.com>
2019-06-07 08:44:46 +0000

    gitpod

--- mainrenderer.js
+++ mainrenderer.js
@@ -1,1 +1,17 @@
-const {app}=require("electron")
+const electron=require("electron")
+const app=electron.app
+const window=electron.BrowserWindow
+var mainWindow=null
+
+const path = require('path')
+const url = require('url')
+
+let createWindow=(w,fileURL,width,height)=>{
+    w=new window({width:width,height:height})
+    w.loadURL(url.format({
+        pathname: path.join(__dirname, ),
+        protocol: 'file:',
+        slashes: true
+    }))
+}
+app.on('ready',createWindow(mainWindow,'/views/index.html',"1024","768"))
